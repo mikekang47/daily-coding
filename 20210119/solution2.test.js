a322098b7b1490a4423017a7b6b73ab99508d88a
mikekang47 <gyeong0308@gmail.com>
2021-01-19 21:20:16 +0900

    Solution 1: used for statement, sort, slice Solution 2: used filter, sort, map

---
+++ 20210119/solution2.test.js
@@ -0,0 +1,9 @@
+test('solution2', () => {
+    const solution2 = require('./solution2');
+
+    const array = [1, 5, 2, 6, 3, 7, 4];
+    const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+
+    expect(solution2(array,commands)).toStrictEqual([5, 6, 3]);
+}) 
+
