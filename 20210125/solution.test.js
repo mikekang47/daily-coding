28f1e6abb26fabc6970831410891779a8a098399
mikekang47 <gyeong0308@gmail.com>
2021-01-25 22:49:27 +0900

    Solve a test about counting words Need to review to get input

---
+++ 20210125/solution.test.js
@@ -0,0 +1,7 @@
+test('solution',() => {
+
+
+    expect(solution("Hello World!")).toStrictEqual(2);
+    expect(solution("I am ironman")).toStrictEqual(3);
+    expect(solution("i'll be back")).toStrictEqual(3);
+  })
