64ab68175fff46897567dc223057a465e5d27df9
mikekang47 <gyeong0308@gmail.com>
2021-02-01 21:22:44 +0900

    Solve a problem with while loop and filter function

--- 20210201/solution.test.js
+++ 20210201/solution.test.js
@@ -1,5 +1,12 @@
-
-
-test('/solution', () => {
+function solution(citations) {
+    let n = 0;
+    while (citations.filter(i => i >= n).length >= n ? true : false) {
+      n++;
+    }
+  
+    return n-1;
+  }
+  
+  test('solution', () => {
     expect(solution([3, 0, 6, 1, 5])).toBe(3);
-})
+  })
