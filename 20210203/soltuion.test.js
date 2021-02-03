b22c83f17339bae645e4630b6e5a23f593ba85c5
mikekang47 <gyeong0308@gmail.com>
2021-02-03 22:42:15 +0900

    Set envrionment and make test case for it

---
+++ 20210203/soltuion.test.js
@@ -0,0 +1,13 @@
+function solution() {
+    const s_rate = [93, 30, 55];
+    const d_rate = [1, 30, 5];
+
+    
+
+    return answer;
+}
+
+test('solution',() => {
+    expect([[93, 30, 55], [1, 30, 5]]).toBe([2, 1]);
+
+})
