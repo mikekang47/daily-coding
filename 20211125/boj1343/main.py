d07b1b9a31f95b8e6ca0340c5f89ba8232027321
mikekang47 <gyeong0308@gmail.com>
2021-11-26 00:18:35 +0900

    solve: boj1343 by greedy algorithm

--- 20211125/boj1343/main.py
+++ 20211125/boj1343/main.py
@@ -1,13 +1,10 @@
 import sys
 
-s = str(input().split('.'))
-arr = []
-temp = ""
-while len(s):
-    if len(s) >= 4:
-        s = s[:4]
-        arr.append("AAAA")
-    elif len(s) >= 2:
-        s = s[:2]
-        arr.append("BB")
-    elif
+s = input()
+s = s.replace("XXXX", "AAAA")
+s = s.replace("XX", "BB")
+
+if "X" in s:
+    print("-1")
+else:
+    print(s)
