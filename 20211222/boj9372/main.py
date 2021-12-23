8d765c0b1ae176882ae2e02e906e1aac91c7d5dc
mikekang47 <gyeong0308@gmail.com>
2021-12-23 20:33:21 +0900

    solve: boj9372

---
+++ 20211222/boj9372/main.py
@@ -0,0 +1,8 @@
+import sys
+t = int(sys.stdin.readline().strip())
+for i in range(t):
+    n, m = map(int, sys.stdin.readline().strip().split())
+    for row in range(m):
+        sys.stdin.readline().strip().split()
+    print(n-1)
+
