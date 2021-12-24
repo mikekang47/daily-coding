4c3b6ebbc47fced4b5b0c90378d7ed5b2ac2cd1f
mikekang47 <gyeong0308@gmail.com>
2021-12-24 22:23:12 +0900

    solve: boj2559 by accumulate sum

---
+++ 20211224/2559/cmake-build-debug/CMakeFiles/2559.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2559"
+  "2559.pdb"
+  "CMakeFiles/2559.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2559.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
