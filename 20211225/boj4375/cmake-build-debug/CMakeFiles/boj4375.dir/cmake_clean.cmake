e77d102ba57ecb30cc82b9e3a23105c1551ac98d
mikekang47 <gyeong0308@gmail.com>
2021-12-25 22:19:55 +0900

    solve: bo4375

---
+++ 20211225/boj4375/cmake-build-debug/CMakeFiles/boj4375.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boj4375.dir/main.cpp.o"
+  "boj4375"
+  "boj4375.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boj4375.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
