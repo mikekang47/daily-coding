ccfc894330fddc650de01969e6a7893a008b2528
mikekang47 <gyeong0308@gmail.com>
2022-01-16 22:20:06 +0900

    Solve: boj4659

---
+++ 20210116/boj4659/main.py
@@ -0,0 +1,18 @@
+while True:
+    s = input()
+    if s == "end":
+        break
+    good = True
+    a = s;
+    for x in "aeiou":
+        a = a.replace(x, "a")
+    for x in "qwrtypsdfghjklzxvbnm":
+        a = a.replace(x, "b")
+    if sum(map(lambda x:s.count(x), "aeiou")) == 0:
+        good = False
+    if a.count("aaa") + a.count("bbb") > 0:
+        good = False
+    if sum(map(lambda x:s.count(x + x), "qwrtyuipasdfghjklzxcvbnm")) > 0:
+        good = False
+    print(("<%s> is acceptable." if good else "<%s> is not acceptable.") % s)
+
