13766150b8d5e9b0e669741bc2ea75bb41bf4e82
mikekang47 <gyeong0308@gmail.com>
2022-01-20 01:59:32 +0900

    Refactor

--- 20220118/boj10709/main.py
+++ 20220118/boj10709/main.py
@@ -1,23 +1,36 @@
-h, w = map(int, input().split())
-graph = []
-for i in range(h):
-    graph.append(list(input()))
-cloud = []
-for y in range(h):
-    idx = None
-    temp = []
-    for x in range(w):
-        if graph[y][x] == 'c':
-            temp.append(0)
-            idx = x
-        else:
-            if idx != None:
-                temp.append(x-idx)
-            else:
-                temp.append(-1)
-    cloud.append(temp)
+# h, w = map(int, input().split())
+# graph = []
+# for i in range(h):
+#     graph.append(list(input()))
+# cloud = []
+# for y in range(h):
+#     idx = None
+#     temp = []
+#     for x in range(w):
+#         if graph[y][x] == 'c':
+#             temp.append(0)
+#             idx = x
+#         else:
+#             if idx != None:
+#                 temp.append(x-idx)
+#             else:
+#                 temp.append(-1)
+#     cloud.append(temp)
+#
+# for row in cloud:
+#     for col in row:
+#         print(col, end=' ')
+#     print()
 
-for row in cloud:
-    for col in row:
-        print(col, end=' ')
-    print()
+h, w = map(int, input().split())
+for _ in range(h):
+    t = -1
+    cloud = input()
+    temp = []
+    for i in range(len(cloud)):
+        if cloud[i] == 'c':
+            t = 0
+        temp.append(str(t))
+        if t >= 0:
+            t += 1
+    print(' '.join(temp))
