f7f225e1e6a7ec58c750cf70168727490c0fc2a9
mikekang47 <gyeong0308@gmail.com>
2022-03-10 01:36:57 +0900

    Solve: boj2839

---
+++ 20220309/boj2839/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 9999);
+        dp[3] = 1;
+        dp[5] = 1;
+        for(int i = 6; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i-3] + 1, dp[i-5] + 1);
+        }
+        if(dp[n] > 9999)
+            System.out.println(-1);
+        else
+            System.out.println(dp[n]);
+    }
+}
