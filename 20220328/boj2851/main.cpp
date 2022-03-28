1ef4fcbc5208a9095fdbf537522e63847a5e759f
mikekang47 <gyeong0308@gmail.com>
2022-03-29 00:46:41 +0900

    Solve: boj2851

--- 20220328/boj2851/main.cpp
+++ 20220328/boj2851/main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
+
 using namespace std;
 int arr[11];
-
 int main() {
+    int sum = 0;
     for(int i = 0; i < 10; i++) {
         cin >> arr[i];
     }
-    int sum = 0;
     for(int i = 0; i < 10; i++) {
         int a = sum;
         int b = sum + arr[i];
-        if(b < 100) {
-            sum = b;
-        } else if(b == 100){
+        if(b == 100) {
             cout << b;
-
+            return 0;
         } else {
-            if(b - 100 > 100 - a) {
-                cout << a;
-
-            } else if(b - 100 == 100 -a) {
-               cout << b;
-
+            if(b < 100) {
+                sum += arr[i];
             } else {
-                cout << b;
+              if(a + b == 200) {
+                  cout << b;
+                  return 0;
+              } else {
+                  if(b - 100 < 100 - a) {
+                      cout << b;
+                      return 0;
+                  } else {
+                      cout << a;
+                      return 0;
+                  }
+              }
             }
         }
     }
+
     return 0;
 }
