5e26adc21d3186c516db92d7c52f16f16be15498
mikekang47 <gyeong0308@gmail.com>
2022-05-20 23:46:29 +0900

    Solve: boj11726

---
+++ 20220519/boj11726/main.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.setrecursionlimit(1004)
+def solution(n):
+    mem = [-1 for i in range(1004)]
+
+    def dp(n):
+        if mem[n] != -1: return mem[n]
+        if n == 0: return 1
+        if n == 1: return 1
+        mem[n] = (dp(n-1) + dp(n-2)) % 10007
+        return mem[n]
+    return dp(n)
+
+n = int(input())
+print(solution(n))
+
+
