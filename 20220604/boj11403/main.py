61d07a81d0e3c41f766f3945c46a16b4cbcef0f8
mikekang47 <gyeong0308@gmail.com>
2022-06-05 23:11:39 +0900

    Update: boj11403

--- 20220604/boj11403/main.py
+++ 20220604/boj11403/main.py
@@ -4,7 +4,7 @@ a=[list(map(int,input().split())) for _ in range(n)]
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            a[i][j] |= a[i][k] and a[k][j]
+            a[i][j] |= a[i][k] & a[k][j]
 for x in a:
     for y in x:
         print(y,end=' ')
