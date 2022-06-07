7babc2a27a80b6b4bbd278e8d68af50533b925e9
mikekang47 <gyeong0308@gmail.com>
2022-06-08 00:51:09 +0900

    Solve: boj10988

---
+++ algorithm_test/boj10988/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string temp = s;
+    reverse(s.begin(), s.end());
+    if(s == temp) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
