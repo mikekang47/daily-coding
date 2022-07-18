a4100eddb1811b7f28b7ce58131da0b57e967671
mikekang47 <gyeong0308@gmail.com>
2022-07-19 00:06:48 +0900

    Generalize code

--- 20220716/boj1764/main.py
+++ 20220716/boj1764/main.py
@@ -1,17 +1,8 @@
-from collections import defaultdict
-n, m = map(int, input().split())
-d = set()
-d2 = set()
-for _ in range(n):
-    d.add(input())
-for _ in range(m):
-    d2.add(input())
-
-
-li = sorted(list(d.intersection(d2)))
-print(len(li))
-for i in li:
-    print(i)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+li = sys.stdin.read().split()
+i = sorted(set(li[:n]) & set(li[n:]))
+print(len(i), *i, sep="\n")
 
 
 
