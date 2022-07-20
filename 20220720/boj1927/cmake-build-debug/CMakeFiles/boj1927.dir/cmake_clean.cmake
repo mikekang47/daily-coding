31c8cf2414a650f1aa29ed483da1270d2b9af38d
mikekang47 <gyeong0308@gmail.com>
2022-07-20 23:53:58 +0900

    Solve: boj1927

---
+++ 20220720/boj1927/cmake-build-debug/CMakeFiles/boj1927.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boj1927.dir/main.cpp.o"
+  "boj1927"
+  "boj1927.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boj1927.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
