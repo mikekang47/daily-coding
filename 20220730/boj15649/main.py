132b3fc26d43ddec3b553ea87f0b2aa43080e9c9
mikekang47 <gyeong0308@gmail.com>
2022-07-30 16:55:24 +0900

    Solve: boj15649

---
+++ 20220730/boj15649/main.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+
+n, m = map(int, input().split())
+for i in list(permutations(range(1, n+1), m)):
+    for j in range(m):
+        print(i[j], end = ' ')
+    print()
