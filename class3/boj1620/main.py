8c3b83f16a2e465376fad55363a9e0e56de4d711
mikekang47 <gyeong0308@gmail.com>
2022-10-02 20:38:48 +0900

    Fix: have better way to solve problem

--- class3/boj1620/main.py
+++ class3/boj1620/main.py
@@ -2,15 +2,11 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 d1 = {}
-d2 = {}
 for i in range(1, n + 1):
     s = sys.stdin.readline()[:-1]
     d1[s] = i
-    d2[i] = s
+    d1[str(i)] = s
 for i in range(m):
     s = sys.stdin.readline()[:-1]
-    try:
-        print(d1[s])
-    except:
-        print(d2[int(s)])
+    print(d1[s])
 
