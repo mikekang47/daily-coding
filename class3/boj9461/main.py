55a1701a9faa4e3c0c6a86732c8c3e786c6bac76
mikekang47 <gyeong0308@gmail.com>
2022-10-05 17:55:02 +0900

    Solve: boj9461

---
+++ class3/boj9461/main.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    dp = [0] * 101
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    n = int(input())
+    for i in range(4, n+1):
+        dp[i] = dp[i-2] + dp[i-3]
+    print(dp[n])
