57e5ebf18dbbb40de673b88e78d2c61d22627f0d
mikekang47 <gyeong0308@gmail.com>
2022-10-06 23:26:13 +0900

    Solve: boj11727

--- class3/boj11727/main.py
+++ class3/boj11727/main.py
@@ -1,7 +1,8 @@
-n = int(input())
-dp = [0 for _ in range(1001)]
+import sys
+n = int(sys.stdin.readline())
+dp = [1 for _ in range(1004)]
 dp[2] = 3
 dp[3] = 5
 for i in range(4, n+1):
-    dp[i] = dp[i-1] + 2 * dp[i-2]
-print(dp[n] % 10007)
+    dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007
+print(dp[n])
