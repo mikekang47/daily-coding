5327789726e097be96dfdd526fae2cf9868bd2b5
mikekang47 <gyeong0308@gmail.com>
2022-11-24 00:08:06 +0900

    Fix

--- 20221120/boj9465/main.py
+++ 20221120/boj9465/main.py
@@ -1,22 +1,27 @@
 import sys
 
 input = sys.stdin.readline
-t = int(input())
 
-while t > 0:
+T = int(input())
+for _ in range(T):
     n = int(input())
-    a1 = list(map(int, input().split()))
-    a2 = list(map(int, input().split()))
-    sumList = [a1, a2]
-    dp = [[0 for _ in range(100000)] for _ in range(2)]
+    stickers = []
 
-    dp[0][0] = sumList[0][0]
-    dp[1][0] = sumList[1][0]
-    dp[0][1] = dp[1][0] + sumList[0][1]
-    dp[1][1] = dp[0][0] + sumList[1][1]
-    dp[1][1] = dp[0][0] + sumList[1][1]
-    for j in range(2, n):
-        dp[0][j] = max(dp[1][j - 1], dp[1][j - 2]) + sumList[0][j]
-        dp[1][j] = max(dp[0][j - 1], dp[0][j - 2]) + sumList[1][j]
-    print(max(dp[0][n - 1], dp[1][n - 1]))
-    t -= 1
+    for _ in range(2):
+        stickers.append(list(map(int, input().rsplit())))
+    dp = [[0 for _ in range(n)] for _ in range(2)]
+
+    if n == 1:
+        print(max(stickers[0][0], stickers[1][0]))
+    elif n == 2:
+        print(max(stickers[0][0] + stickers[1][1],
+                  stickers[1][0] + stickers[0][1]))
+    else:
+        dp[0][0], dp[1][0] = stickers[0][0], stickers[1][0]
+        dp[0][1], dp[1][1] = stickers[0][1] + stickers[1][0], stickers[0][0] + stickers[1][1]
+
+        for i in range(2, n):
+            dp[0][i] = max(dp[1][i - 1], dp[0][i - 2], dp[1][i - 2]) + stickers[0][i]
+            dp[1][i] = max(dp[0][i - 1], dp[1][i - 2], dp[0][i - 2]) + stickers[1][i]
+
+        print(max(dp[0][n - 1], dp[1][n - 1]))
