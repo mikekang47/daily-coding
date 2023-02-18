a82815b383f742798c621975bda264f08d09a167
mikekang47 <gyeong0308@gmail.com>
2023-02-18 23:58:23 +0900

    Solve: boj2343

---
+++ 20230218/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(start < arr[i]) start = arr[i];
+            end = end + arr[i];
+        }
+        while(start <= end) {
+            int middle = (start + end) / 2;
+            int sum = 0;
+            int count = 0;
+            for(int i = 0;i < n; i++) {
+                if(sum + arr[i] > middle) {
+                    count++;
+                    sum = 0;
+                }
+                sum += arr[i];
+            }
+            if(sum != 0) {
+                count++;
+            }
+            if(count > m) {
+                start = middle + 1;
+            
+            } else {
+                end = middle - 1;
+            }
+        }
+        System.out.println(start);
+    }
+}
