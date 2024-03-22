c1bfd597d712dfe04b69b71083d81bf0fc429dfb
sihoo kang <sihookang47@gmail.com>
2024-03-22 22:38:10 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 396 ms, Memory: 111568 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+
+parent = [i for i in range(N + 1)]
+
+
+def find(a):
+    if parent[a] == a:
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a == b:
+        return
+    parent[a] = b
+
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    union(a, b)
+
+for i in range(N + 1):
+    parent[i] = find(i)
+
+print(len(set(parent[1:])))
